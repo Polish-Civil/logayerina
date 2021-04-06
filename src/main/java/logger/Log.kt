f6320a2cd22d2fac460cf0b4c5f88251bf6407b9
dqw4w9wgxcq <dqw4w9wyxcq@gmail.com>
2021-04-06 00:22:48 -0700

    asdf

--- src/main/java/logger/Log.kt
+++ src/main/java/logger/Log.kt
@@ -1,71 +1,96 @@
 package logger
 
-import java.awt.Color
-
-/*
-requirements
-
-filter by class
-
-filter by package
-
-unfilter by package
-
-unfilter by class
-
-x override class
-
-groups
-*/
+import org.slf4j.event.Level
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
 
 fun main() {
-    Log.info("ook")
+    try {
+        throw Exception("fkdsafkdf")
+    } catch (e: Exception) {
+        Log.error("asdf", e)
+    }
+
+
+    Log.debug("asdf")
 }
 
 object Log {
     @JvmStatic
-     var config = Config()
+    var config = Config()
 
-    enum class Level(val color: Color) {
-        DEBUG(Color.PINK), INFO(Color.WHITE), FINE(Color.GREEN), WARN(Color.YELLOW), ERROR(Color.ORANGE), FATAL(Color.RED),
-    }
-
-    fun log(level: Level, msg: Any?, classLabel: Class<*> = CallingClass.get()) {
-        val className = (classLabel).name.split("$")[0]
-        for (filter in config.filters) {
-
+    fun log(level: Level, message: Any?, throwable: Throwable?, classLabel: Class<*>) {
+        var mostSpecificPolicy = Policy(path = "", config.defaultLogLevel)
+        val topClassName = (classLabel).name.split("$")[0]//handles anon classes unless somehow a package has a "$"
+        for (policy in config.logPolicies) {
+            if (topClassName.contains(policy.path) && mostSpecificPolicy.path.length <= policy.path.length) {//<= because prefer last
+                mostSpecificPolicy = policy
+            }
         }
+
+        if (mostSpecificPolicy.logLevel < level) return
+
+        val time = LocalTime.now().truncatedTo(ChronoUnit.SECONDS)
+        val packageInitials = classLabel.packageName.split(".").map { it[0] }.joinToString(separator = ".")
+        val classSimpleName = classLabel.simpleName
+        var fullMessage = if(message is Throwable){
+            message.stackTraceToString()
+        }else{
+            message.toString()
+        }
+        if (throwable != null) {
+            fullMessage += "\n" + throwable.stackTraceToString()
+        }
+        val formattedMessage = "$time $level $packageInitials.$classSimpleName $fullMessage"
+
+        println(formattedMessage)
     }
 
     @JvmStatic
     @JvmOverloads
-    fun info(msg: Any?, classLabel: Class<*> = CallingClass.get()) {
-        log(Level.INFO, msg, classLabel)
+    fun debug(message: Any?, classLabel: Class<*> = CallingClass.get(3)) {
+        log(Level.DEBUG, message, null, classLabel)
     }
 
-    class Config(level: Level = Level.INFO) {
-        internal val filters = mutableListOf<Pair<Level, String>>()
+    @JvmStatic
+    @JvmOverloads
+    fun info(message: Any?, classLabel: Class<*> = CallingClass.get(3)) {
+        log(Level.INFO, message, null, classLabel)
+    }
 
-        fun set(level:Level, classPackageName:String){
+    @JvmStatic
+    @JvmOverloads
+    fun warn(message: Any?, throwable: Throwable? = null, classLabel: Class<*> = CallingClass.get(3)) {
+        log(Level.WARN, message, throwable, classLabel)
+    }
 
+    @JvmStatic
+    @JvmOverloads
+    fun error(message: Any?, throwable: Throwable? = null, classLabel: Class<*> = CallingClass.get(3)) {
+        log(Level.ERROR, message, throwable, classLabel)
+    }
+
+    data class Policy(val path: String, val logLevel: Level)
+
+    class Config(val defaultLogLevel: Level = Level.INFO) {
+        internal val logPolicies = mutableListOf<Policy>()
+
+        fun addPolicy(logPolicy: Policy) {
+            logPolicies.add(logPolicy)
         }
 
-        fun set(level:Level, clazz:Class<*>){
-            set(level, clazz.name)
+        fun addPolicy(path: String, level: Level) {
+            addPolicy(Policy(path, level))
         }
 
-//        fun enable(`package`: Package) {
-//            enable(`package`.name)
-//        }
-//
-//        fun disable(`package`: Package) {
-//            disable(`package`.name)
-//        }
+        fun addPolicy(clazz: Class<*>, level: Level) {
+            addPolicy(clazz.name, level)
+        }
     }
 
     private object CallingClass : SecurityManager() {
-        fun get(): Class<*> = classContext[1]
+        fun get(index:Int): Class<*> {
+            return classContext[index]
+        }
     }
 }
-
-
